41472010ac988db5679780b4acbeb498372d1e4d
kip <zzerocks@gmail.com>
2022-10-20 22:55:59 +0300

    Кнопка оплаты

--- events/pay_views.py
+++ events/pay_views.py
@@ -1,6 +1,7 @@
 from django import views
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from events.models import Event, Participant
 
@@ -21,6 +22,11 @@ class CreatePay(views.View):
     def get(request, event_id, p_id):
         event = Event.objects.get(id=event_id)
         participant = Participant.objects.get(id=p_id)
+        if participant.paid:
+            return redirect('pay_ok', event_id)
+        amount = event.price
+        order_id = f"e{event_id}_p{p_id}_{participant.last_name}_{participant.first_name}"
+        success_uri = request.build_absolute_uri(reverse('pay_ok', args=(event_id,)))
         return render(
             request=request,
             template_name='events/event/pay.html',
@@ -28,5 +34,21 @@ class CreatePay(views.View):
                 'title': f'{participant.last_name} {participant.first_name}',
                 'event': event,
                 'participant': participant,
+                'order_id': order_id,
+                'amount': amount,
+                'success_uri': success_uri,
+            }
+        )
+
+
+class PayOk(views.View):
+    @staticmethod
+    def get(request, event_id):
+        event = Event.objects.get(id=event_id)
+        return render(
+            request=request,
+            template_name='events/event/pay-ok.html',
+            context={
+                'event': event,
             }
         )
