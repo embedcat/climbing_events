b0aad4bc77e2983b0238c202fc304d4585d4578a
kip <kip@kit-invest.ru>
2022-12-06 16:28:32 +0300

    помощь - оплата

--- events/about_views.py
+++ events/about_views.py
@@ -1,6 +1,8 @@
 from django import views
 from django.shortcuts import render
 
+from events.pay_views import get_notify_link
+
 
 class AboutView(views.View):
     @staticmethod
@@ -13,4 +15,7 @@ class HelpView(views.View):
     @staticmethod
     def get(request, type):
         return render(request=request,
-                      template_name=f"events/about/{type}.html")
+                      template_name=f"events/about/{type}.html",
+                      context={
+                        'notify_link': get_notify_link(request=request) if type == "payment" else None,
+                      })
